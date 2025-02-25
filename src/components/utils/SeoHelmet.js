86cbccc208a657943e5355ae0259d4535a3dabcd
Martin-Po <85139462+Martin-Po@users.noreply.github.com>
2025-02-25 20:42:22 -0300

    Deleted icon link in SeoHelmet.js

--- src/components/utils/SeoHelmet.js
+++ src/components/utils/SeoHelmet.js
@@ -23,11 +23,9 @@ export const SeoHelmet = ({ lang, page }) => {
         <meta name="description" content={metaTexts[lang][page].description} />
         <meta property="og:title" content={metaTexts[lang][page].ogTitle} />
         <meta property="og:description" content={metaTexts[lang][page].ogDescription} />
-        <meta property="og:image" content={`${process.env.PUBLIC_URL}/images/logo_light.png`} />        <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metaTexts[lang][page].twTitle} />
         <meta name="twitter:description" content={metaTexts[lang][page].twDescription} />
-        <meta name="twitter:image" content={`${process.env.PUBLIC_URL}/images/logo_light.png`} />
       </Helmet>
     );
   };
